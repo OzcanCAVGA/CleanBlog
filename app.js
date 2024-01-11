72e6a733f5eef0a6c87e226da9eea6c9a0e75ff2
Özcan ÇAVGA <81030695+OzcanCAVGA@users.noreply.github.com>
2024-01-11 18:16:30 +0300

    Add routes and templates for about, add_post, and post pages

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 const express = require('express');
+const app = express();
+const path = require("path")
+const ejs = require('ejs');
+app.use(express.static('public'))
 
-const app = express()
+//TEMPLATE ENGINE
+app.set('view engine','ejs')
 
 app.get("/",(req,res)=>{
-
-    const blog ={
-        id:1,
-        title:"Blog title",
-        description:"Blog description"
-    }
-    res.send(blog)
+    res.render('index')
 })
+
+app.get("/about",(req,res)=>{
+    res.render('about')
+})
+app.get("/add_post",(req,res)=>{
+    res.render('add_post')
+})
+app.get("/post",(req,res)=>{
+    res.render('post')
+})
+
 
 const port = 4000;
 
