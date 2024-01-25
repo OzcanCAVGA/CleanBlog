26e9b18b40a8e40c582c1910e1178cb1b0a0c8fa
Özcan ÇAVGA <81030695+OzcanCAVGA@users.noreply.github.com>
2024-01-26 00:20:04 +0300

    Add MongoDB connection and Post model

--- app.js
+++ app.js
@@ -1,29 +1,59 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 const path = require("path")
 const ejs = require('ejs');
-app.use(express.static('public'))
+const Post = require('./models/Post');
+
+
+const app = express();
+
+app.use(express.static('public'));
+
+//connect DB
+main().catch(err => console.log(err));
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/cleanblog-test-db'), {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+}
 
 //TEMPLATE ENGINE
-app.set('view engine','ejs')
+app.set('view engine', 'ejs')
 
-app.get("/",(req,res)=>{
-    res.render('index')
+//MIDDLEWARE
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+//ROUTES
+app.get("/", async (req, res) => {
+    const posts = await Post.find({})
+    res.render('index', {
+        posts
+    })
 })
 
-app.get("/about",(req,res)=>{
+app.get("/about", (req, res) => {
     res.render('about')
 })
-app.get("/add_post",(req,res)=>{
+app.get("/post", (req, res) => {
+    res.render('post')
+})
+
+
+app.get("/add_post", (req, res) => {
     res.render('add_post')
 })
-app.get("/post",(req,res)=>{
-    res.render('post')
+
+app.post("/add_post", async (req, res) => {
+    await Post.create(req.body)
+    res.redirect('/')
 })
 
 
 const port = 4000;
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Sunucumuz ${port} portunda calisiyor.`)
 })
